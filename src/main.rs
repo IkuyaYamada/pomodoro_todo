401566b576ebd97612945f6a1ba41c91aac742da
Win-Ikuya <dalyson.roman345@gmail.com>
2023-11-05 06:30:29 +0900

    hello world to warp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    // GET /hello に対して "Hello, world!" と応答するルートを作成します。
+    let hello = warp::path("hello")
+        .map(|| warp::reply::html("Hello, world!"));
+
+    // Warpサーバーを起動します。このサーバーはlocalhostのポート3030でリクエストを待ち受けます。
+    warp::serve(hello)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }
